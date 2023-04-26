2dccaf3f0678dac00367b8fb4ee43a837bec8dc3
dalmoling <107952388+dalmoling@users.noreply.github.com>
2023-04-26 08:59:42 -0300

    Update bucketsort.c

--- bucketsort.c
+++ bucketsort.c
@@ -1,72 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void bucketSort(int arr[], int n) {
-    // Define o número de baldes
-    const int num_buckets = 10;
+// Definindo o tamanho máximo do array
+#define MAX 10
+ 
+// Definindo o número de baldes
+#define NUM_BUCKETS 10
 
-    // Cria os baldes
-    int buckets[num_buckets][n];
+// Definindo a estrutura do balde
+struct Node {
+    int value;
+    struct Node* next;
+};
 
-    // Inicializa os contadores de cada balde para zero
-    int bucket_counters[num_buckets] = {0};
-
-    // Calcula o valor máximo do array
-    int max_val = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > max_val) {
-            max_val = arr[i];
-        }
+// Função para criar um novo nó
+struct Node* newNode(int value) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->value = value;
+    node->next = NULL;
+    return node;
+}
+ 
+// Função para ordenar usando o Bucket Sort
+void bucketSort(int array[], int n) {
+    // Criando o array de baldes
+    struct Node* buckets[NUM_BUCKETS];
+    for (int i = 0; i < NUM_BUCKETS; i++) {
+        buckets[i] = NULL;
     }
-
-    // Calcula o tamanho de cada balde
-    double range = (double) (max_val + 1) / num_buckets;
-
-    // Coloca os valores no balde correspondente
+    
+    // Adicionando os valores nos baldes
     for (int i = 0; i < n; i++) {
-        int bucket_idx = (int) (arr[i] / range);
-        buckets[bucket_idx][bucket_counters[bucket_idx]] = arr[i];
-        bucket_counters[bucket_idx]++;
-    }
-
-    // Ordena cada balde individualmente
-    for (int i = 0; i < num_buckets; i++) {
-        for (int j = 0; j < bucket_counters[i]; j++) {
-            int temp = buckets[i][j];
-            int k = j - 1;
-            while (k >= 0 && buckets[i][k] > temp) {
-                buckets[i][k+1] = buckets[i][k];
-                k--;
+        int index = NUM_BUCKETS * array[i] / (MAX + 1);
+        struct Node* node = newNode(array[i]);
+        if (buckets[index] == NULL) {
+            buckets[index] = node;
+        } else {
+            struct Node* current = buckets[index];
+            while (current->next != NULL) {
+                current = current->next;
             }
-            buckets[i][k+1] = temp;
+            current->next = node;
         }
     }
-
-    // Combina os baldes ordenados em um único array
-    int idx = 0;
-    for (int i = 0; i < num_buckets; i++) {
-        for (int j = 0; j < bucket_counters[i]; j++) {
-            arr[idx] = buckets[i][j];
-            idx++;
+ 
+    // Ordenando os baldes
+    for (int i = 0; i < NUM_BUCKETS; i++) {
+        struct Node* current = buckets[i];
+        while (current != NULL) {
+            printf("%d ", current->value);
+            current = current->next;
         }
     }
 }
 
+// Função principal
 int main() {
-    int arr[] = {3, 8, 2, 1, 5, 6, 4, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Array original: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    int array[MAX] = {7, 10, 3, 8, 2, 6, 1, 5, 9, 4};
+    printf("Array antes da ordenação:\n");
+    for (int i = 0; i < MAX; i++) {
+        printf("%d ", array[i]);
     }
-
-    bucketSort(arr, n);
-
-    printf("\nArray ordenado: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-
+    printf("\n\nArray depois da ordenação:\n");
+    bucketSort(array, MAX);
+    printf("\n");
     return 0;
 }
